6ddcfd6805132b8a93a9d202428181117e6abf82
Mika <wemi99@outlook.de>
2021-05-11 20:33:50 +0200

    M3 Aufgabe 1-6

--- resources/views/m2_menue.blade.php
+++ resources/views/m2_menue.blade.php
@@ -19,9 +19,24 @@
         let elements = menu["el"];
 
         for (let i=0;i<elements.length;i++){
+            let link = create("a");
             let e = create("li")
             e.innerText = elements[i].name;
-            ul.append(e);
+
+            // add link to element
+            link.addEventListener("click", function(){
+                elements[i].a();
+            });
+            // add hover color
+            link.addEventListener("mouseover", function(){
+              e.style.cssText = 'color:' +  elements[i].hoverColor();
+            });
+            link.addEventListener("mouseout", function(){
+                e.style.cssText = 'color: inherit';
+            });
+
+            link.append(e);
+            ul.append(link);
 
             // if submenu exists
             if (elements[i].sub === true){
@@ -31,8 +46,17 @@
                 for (j=1; j<=elements[i].anzahl;j++)
                 {
                     let sub_e = create("li");
+                    let sub_link = create("a");
                     sub_e.innerText = elements[i+j].name;
-                    sub_ul.append(sub_e);
+                    console.log(j);
+                    sub_link.addEventListener("mouseover", function(){
+                        sub_e.style.cssText = 'color:' +  elements[j].hoverColor();
+                    });
+                    sub_link.addEventListener("mouseout", function(){
+                        sub_e.style.cssText = 'color: inherit';
+                    });
+                    sub_link.append(sub_e);
+                    sub_ul.append(sub_link);
                 }
                 ul.append(sub_ul);
                 i += elements[i].anzahl;
@@ -46,14 +70,15 @@
     let menu = {
         el:
         [
-            {name:"Home", link: "home"},
-            {name: "Kategorien", link: "kategorien", sub:false},
-            {name: "Verkaufen", link: "verkaufen", sub:false},
-            {name: "Unternehmen", link:"unternehmen", sub:true, anzahl: 2},
-            {name: "Philosophie", link:"Philosophie", sub:false},
-            {name: "Karriere", link: "karriere", sub: false},
+            {name:"Home", link: "home", a: function(){return location.href = "/"}, hoverColor: function(){return "#fc0303"}},
+            {name: "Kategorien", link: "kategorien",a: function(){return location.href = "/articles"}, hoverColor: function(){return "#fc1c03"}, sub:false},
+            {name: "Verkaufen", link: "verkaufen",a: function(){return location.href = "/newarticle"}, hoverColor: function(){return "#fc3503"}, sub:false},
+            {name: "Unternehmen", link:"unternehmen",a: function(){return location.href = "#"}, hoverColor: function(){return "#fc4a03"}, sub:true, anzahl: 2},
+            {name: "Philosophie", link:"Philosophie",a: function(){return location.href = "#"},hoverColor: function(){return "#fc6b03"}, sub:false},
+            {name: "Karriere", link: "karriere",a: function(){return location.href = "#"},hoverColor: function(){return "#fc9803"}, sub: false},
         ]
     };
+
 
     createMenu(menu);
 
