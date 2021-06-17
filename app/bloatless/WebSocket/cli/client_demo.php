191388ddb8ea51af7734625eb7a7cc2e3b5107d9
Mika <wemi99@outlook.de>
2021-06-18 00:36:47 +0200

    M5 Aufgaben

---
+++ app/bloatless/WebSocket/cli/client_demo.php
@@ -0,0 +1,42 @@
+<?php
+//ini_set('display_errors', 1);
+//error_reporting(E_ALL);
+//
+//require __DIR__ . '/../src/Client.php';
+//
+//$clients = [];
+//$testClients = 30;
+//$testMessages = 500;
+//for ($i = 0; $i < $testClients; $i++) {
+//    $clients[$i] = new \Bloatless\WebSocket\Client;
+//    $clients[$i]->connect('127.0.0.1', 8000, '/demo', 'foo.lh');
+//}
+//usleep(5000);
+//
+//$payload = json_encode([
+//    'action' => 'echo',
+//    'data' => 'dos',
+//]);
+//
+//for ($i = 0; $i < $testMessages; $i++) {
+//    $clientId = rand(0, $testClients-1);
+//    $clients[$clientId]->sendData($payload);
+//    usleep(5000);
+//}
+//usleep(5000);
+require __DIR__ . '/../src/Client.php';
+$client = new \Bloatless\WebSocket\Client;
+
+//$client->connect('127.0.0.1', 8000, '/demo');
+//$client->sendData(json_encode([
+//            'action' => 'echo',
+//            'data' => 'Websockets']
+//    )
+//);
+
+$client->connect('127.0.0.1', 8000, '/demo');
+$client->sendData(json_encode([
+            'action' => 'echo',
+            'data' => '{"id":2, "message":"hidden Text"}']
+    )
+);
